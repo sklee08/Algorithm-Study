fbd000b2aa52ed24bf7b683a7e62f05d7bd66bc4
sklee08 <39113006+sklee08@users.noreply.github.com>
2020-01-28 11:23:41 +0900

    Add files via upload

---
+++ Algorithm/BOJ/Problem_BOJ_10807.java
@@ -0,0 +1,24 @@
+package algorithm_study;
+import java.util.Scanner;
+
+public class Problem_BOJ_10807 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		int[]arr = new int[N];
+		for(int i =0; i< N; i++) {
+			arr[i] = scan.nextInt();
+		}
+		int find = scan.nextInt();
+		int ret = 0;
+		for(int i = 0; i < N; i++) {
+			if(arr[i] == find) {
+				ret++;
+			}
+		}
+		System.out.println(ret);
+	}
+
+}
---
+++ Algorithm/BOJ/Problem_BOJ_10808.java
@@ -0,0 +1,19 @@
+package algorithm_study;
+
+import java.util.Scanner;
+
+public class Problem_BOJ_10808 {
+	public static void main(String [] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String s = scan.next();
+		int[] alpa = new int[26];
+		for(int i = 0; i < s.length(); i++) {
+			alpa[s.charAt(i)-'a']++;
+		}
+		
+		for(int i =0; i< 26; i++) {
+			System.out.print(alpa[i] + " ");
+		}
+	}
+}
