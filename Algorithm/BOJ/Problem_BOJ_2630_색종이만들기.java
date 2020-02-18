c0eb4850838e1a8a01b7564fff6a65b7c3a32e56
sklee08 <39113006+sklee08@users.noreply.github.com>
2020-02-18 13:44:00 +0900

    Add files via upload

---
+++ Algorithm/BOJ/Problem_BOJ_2630_색종이만들기.java
@@ -0,0 +1,87 @@
+package algo_study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Problem_BOJ_2630_색종이만들기 {
+	
+	static int N;
+	static int white;
+	static int blue;
+	static int[][] origin;
+	
+	public static boolean isAllSame(int[][]arr, int n) {
+		int data = arr[0][0];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j< n; j++) {
+				if(arr[i][j] != data) {
+					return false;
+				}
+			}
+		}	
+		if(data == 0) {
+			white++;
+		}
+		else {
+			blue++;
+		}
+		return true;
+	}
+	
+	public static void print(int[][]arr, int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.println(Arrays.toString(arr[i]));
+		}
+		System.out.println();
+	}
+	
+	public static void divide(int[][] arr, int n) {
+		if(n == 1) {
+			int data = arr[0][0];
+			if(data == 0) {
+				white++;
+			}else {
+				blue++;
+			}
+			return;
+		}else {
+			if(!isAllSame(arr, n)) {
+				int div = n / 2;
+				for(int i = 0; i < n; i+=div) {
+					for(int j = 0; j < n; j+=div) {
+						int [][]newArr = new int[div][div];
+						for(int k = 0; k < div; k++) {
+							for(int l = 0; l < div; l++) {
+								newArr[k][l] = arr[i+k][j+l];
+							}
+						}
+						//print(newArr, div);
+						divide(newArr, div);
+					}
+				}
+			}
+		}		
+	}
+	
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		origin = new int[N][N];
+		for(int i = 0; i < N; i++) {
+			String tmp = br.readLine();
+			StringTokenizer st = new StringTokenizer(tmp, " ");
+			for(int j = 0; j < N; j++) {
+				origin[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		divide(origin, N);
+		System.out.println(white);
+		System.out.println(blue);
+	}
+}
+
