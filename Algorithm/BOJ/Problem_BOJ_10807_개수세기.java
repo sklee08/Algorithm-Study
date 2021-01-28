6586be658b74c8493b5bb66b5a93a94417198590
sklee08 <39113006+sklee08@users.noreply.github.com>
2021-01-28 23:27:20 +0900

    Update Problem_BOJ_10807_개수세기.java

--- Algorithm/BOJ/Problem_BOJ_10807_개수세기.java
+++ Algorithm/BOJ/Problem_BOJ_10807_개수세기.java
@@ -1,7 +1,7 @@
 package algorithm_study;
 import java.util.Scanner;
 
-public class Problem_BOJ_10807 {
+public class Problem_BOJ_10807 { 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
